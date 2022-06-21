ac5556008ab7f4928f2065e295e3f8d8d4950ccc
MinhTruong <minhcm2@gmail.com>
2022-06-21 17:05:59 +0700

    upload flie run_bukernetes.sh and add flie depoyment

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
+# Create secret to connect repository image docker private
+kubectl create secret generic key \
+    --from-file=.dockerconfigjson=<path/to/.docker/config.json> \
+    --type=kubernetes.io/dockerconfigjson
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl apply -f deployment_project4.yaml
+kubectl expose deployment project-4 --port=80 --name=service-project-4
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment project-4 --type=LoadBalancer --port=8080 --target-port=80
