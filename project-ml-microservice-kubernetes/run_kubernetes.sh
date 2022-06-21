1b5609f0d7ce33c9c2921bfe0b02b6f1f0d9dc4e
MinhTruong <minhcm2@gmail.com>
2022-06-21 22:21:29 +0700

    update code

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -3,7 +3,7 @@
 # Step 1:
 # Create secret to connect repository image docker private
 kubectl create secret generic key \
-    --from-file=.dockerconfigjson=<path/to/.docker/config.json> \
+    --from-file=.dockerconfigjson="/root/.docker/config.json" \
     --type=kubernetes.io/dockerconfigjson
 # Step 2
 # Run the Docker Hub container with kubernetes
@@ -14,4 +14,4 @@ kubectl expose deployment project-4 --port=80 --name=service-project-4
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment project-4 --type=LoadBalancer --port=8080 --target-port=80
+kubectl expose deployment project-4 --type=LoadBalancer --port=9000 --target-port=80
